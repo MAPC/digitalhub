5b3836afc2d9b10d18b8c784b040a147d595f23e
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-09-12 12:57:35 -0400

    Add Language Selection Menu
    * Add menu to the header for selecting the language of the site.
    * Clean up i18n configuration white space

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -2,12 +2,8 @@
 
 Refinery::I18n.configure do |config|
   config.default_locale = :en
-
   config.current_locale = :en
-
   config.default_frontend_locale = :en
-
   config.frontend_locales = [:en, :pt, :es, :"zh-CN", :fr]
-
   config.locales = {:en=>"English", :pt=>"Português", :es=>"Español", :"zh-CN"=>"简体中文", :fr=>"Français" }
 end
