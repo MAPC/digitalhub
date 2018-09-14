8227084d47d60662d86d8b115fbfd90aae6a891f
Ian Hill <ihill@mapc.org>
2018-09-14 11:08:14 -0400

    Standardizing events index page to Rails

--- vendor/extensions/events/app/controllers/refinery/events/events_controller.rb
+++ vendor/extensions/events/app/controllers/refinery/events/events_controller.rb
@@ -8,6 +8,15 @@ module Refinery
       def index
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @event in the line below:
+        now = DateTime.now
+        now_nozone = DateTime.new(now.year, now.month, now.day, now.hour, now.minute, now.second)
+        in_next_year, next_month = (now_nozone.month + 1).divmod(12)
+        year = in_next_year > 0 ? now_nozone.year + 1 : now_nozone.year
+        beginning_of_next_month = DateTime.new(year, next_month, 1, 0, 0, 0)
+        end_of_next_month = DateTime.new(year, next_month, -1, -1, -1, -1)
+
+        @upcoming_events = ::Refinery::Events::Event.where(start: now_nozone..beginning_of_next_month)
+        @events_next_month = ::Refinery::Events::Event.where(start: beginning_of_next_month..end_of_next_month)
         present(@page)
       end
 
