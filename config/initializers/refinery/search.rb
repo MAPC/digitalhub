a0275b9f2dbec211f40b2330b6e5d37acbfcc1a4
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-09-18 10:58:13 -0400

    Allow for Searching of Events
    * Add SEO metadata fields to events so that users can customzie the information that appears in search results in relation to the event.
    * Make the event extension searchable
    * Add form for filling out the SEO metadata to the admin interface.

--- config/initializers/refinery/search.rb
+++ config/initializers/refinery/search.rb
@@ -1,5 +1,5 @@
 Refinery::Search.configure do |config|
-  config.enable_for = ["Refinery::Page"]
+  config.enable_for = ["Refinery::Page", "Refinery::Events::Event"]
   config.page_url = "/search"
   config.results_per_page = 10
 end
--- vendor/extensions/events/app/models/refinery/events/event.rb
+++ vendor/extensions/events/app/models/refinery/events/event.rb
@@ -1,20 +1,13 @@
 module Refinery
   module Events
     class Event < Refinery::Core::BaseModel
-      self.table_name = 'refinery_events'
-
-
       extend Mobility
+      self.table_name = 'refinery_events'
+      acts_as_indexed :fields => [:title, :description]
+      is_seo_meta
       translates :title, :description, :accessibility_note, :translation_note
-
       validates :title, :presence => true, :uniqueness => true
-
       belongs_to :image, :class_name => '::Refinery::Image'
-
-      # To enable admin searching, add acts_as_indexed on searchable fields, for example:
-      #
-      #   acts_as_indexed :fields => [:title]
-
     end
   end
 end
