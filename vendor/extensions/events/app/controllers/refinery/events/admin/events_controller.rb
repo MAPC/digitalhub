001589682bdbd46859729f58ea557cafab1bc3b7
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-09-21 12:03:22 -0400

    Resolve #36 Event SEO Meta Descriptions
    * Whitelist the seo_meta parameters so that when a user submits an update to an event the app will actually save the SEO meta parameters.

--- vendor/extensions/events/app/controllers/refinery/events/admin/events_controller.rb
+++ vendor/extensions/events/app/controllers/refinery/events/admin/events_controller.rb
@@ -9,7 +9,7 @@ module Refinery
 
         # Only allow a trusted parameter "white list" through.
         def event_params
-          params.require(:event).permit(:title, :description, :start, :end, :image_id, :event_type, :address, :city, :state, :zipcode, :registration_link, :accessibility_note, :translation_note)
+          params.require(:event).permit(:title, :description, :start, :end, :image_id, :event_type, :address, :city, :state, :zipcode, :registration_link, :accessibility_note, :translation_note, :browser_title, :meta_description)
         end
       end
     end
