e00e887ab1de94d668bf8a2cbee088756fc04ae9
Ian Hill <ihill@mapc.org>
2018-09-25 16:07:38 -0400

    First pass at accessibility for navigation drawer

--- app/assets/javascripts/drawer.js
+++ app/assets/javascripts/drawer.js
@@ -4,8 +4,14 @@ function toggleDrawer() {
   if (page.classList.contains("active")) {
     page.classList.remove("active");
     drawer.classList.remove("active");
+    // Drawer starts out aria-hidden
+    // Hidden removes the menu from the tab order until the menu is opened
+    drawer.hidden = true;
+    drawer.setAttribute('aria-hidden', 'true');
   } else {
     page.classList.add("active");
     drawer.classList.add("active");
+    drawer.hidden = false;
+    drawer.setAttribute('aria-hidden', 'false');
   }
 }
