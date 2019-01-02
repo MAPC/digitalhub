7b6e2afd222db4941bf90a877b59667e39688bbd
Eric Youngberg <eyoungberg@mapc.org>
2019-01-02 15:50:36 -0500

    Remove audio recorder if not fully supported

--- app/javascript/audio-recorder.js
+++ app/javascript/audio-recorder.js
@@ -1,63 +1,74 @@
 import { DirectUpload } from "activestorage"
 
-function responseFileName (codec) {
-  const fileTypes = {
-    'audio/wav': 'wav',
-    'audio/webm;codecs=opus': 'webm',
-    'audio/ogg;codecs=opus': 'ogg'
-  };
+(() => {
 
+const fileTypes = {
+  'audio/wav': 'wav',
+  'audio/ogg;codecs=opus': 'ogg',
+};
+
+function responseFileName (codec) {
   return `response-${Date.now()}.${fileTypes[codec]}`;
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-  const input = document.querySelector('#story_audio')
-  const recordButton = document.getElementById("recordButton");
-  const stopButton = document.getElementById("stopButton");
-  let recorder
+  const audioContainer = document.querySelector('#audio-recording')
+  const audioUnsupported = Object.keys(fileTypes).every(type => !MediaRecorder.isTypeSupported(type));
 
-  const uploadFile = (file) => {
-    const url = input.dataset.directUploadUrl
-    const upload = new DirectUpload(file, url)
+  if (audioUnsupported || !audioContainer) {
+    audioContainer.parentNode.removeChild(audioContainer);
+  }
+  else {
+    const input = audioContainer.querySelector('#story_audio')
+    const recordButton = audioContainer.getElementById("recordButton");
+    const stopButton = audioContainer.getElementById("stopButton");
+    let recorder = null;
 
-    upload.create((error, blob) => {
-      if (error) {
-        console.log('There was an error!')
-      } else {
-        // Add an appropriately-named hidden input to the form with a
-        //  value of blob.signed_id so that the blob ids will be
-        //  transmitted in the normal upload flow
-        const hiddenField = document.createElement('input')
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("value", blob.signed_id);
-        hiddenField.name = input.name
-        document.querySelector('form').appendChild(hiddenField)
-      }
+    const uploadFile = (file) => {
+      const url = input.dataset.directUploadUrl
+      const upload = new DirectUpload(file, url)
+
+      upload.create((error, blob) => {
+        if (error) {
+          console.log('There was an error!')
+        } else {
+          // Add an appropriately-named hidden input to the form with a
+          //  value of blob.signed_id so that the blob ids will be
+          //  transmitted in the normal upload flow
+          const hiddenField = document.createElement('input')
+          hiddenField.setAttribute("type", "hidden");
+          hiddenField.setAttribute("value", blob.signed_id);
+          hiddenField.name = input.name
+          document.querySelector('form').appendChild(hiddenField)
+        }
+      })
+    }
+
+    input.addEventListener('change', (event) => {
+      Array.from(input.files).forEach(file => uploadFile(file))
+      input.value = null
+    })
+
+    recordButton.addEventListener('click', () => {
+      // Request permissions to record audio
+      navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+        recorder = new MediaRecorder(stream)
+        recorder.addEventListener('dataavailable', e => {
+          debugger;
+          const file = new File([e.data], responseFileName(e.data.type), {type: e.data.type})
+          uploadFile(file)
+        })
+        recorder.start()
+      })
+    })
+
+    stopButton.addEventListener('click', () => {
+      // Stop recording
+      recorder.stop()
+      // Remove “recording” icon from browser tab
+      recorder.stream.getTracks().forEach(i => i.stop())
     })
   }
-
-  input.addEventListener('change', (event) => {
-    Array.from(input.files).forEach(file => uploadFile(file))
-    input.value = null
-  })
-
-  recordButton.addEventListener('click', () => {
-    // Request permissions to record audio
-    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-      recorder = new MediaRecorder(stream)
-      recorder.addEventListener('dataavailable', e => {
-        debugger;
-        const file = new File([e.data], responseFileName(e.data.type), {type: e.data.type})
-        uploadFile(file)
-      })
-      recorder.start()
-    })
-  })
-
-  stopButton.addEventListener('click', () => {
-    // Stop recording
-    recorder.stop()
-    // Remove “recording” icon from browser tab
-    recorder.stream.getTracks().forEach(i => i.stop())
-  })
 });
+
+})();
