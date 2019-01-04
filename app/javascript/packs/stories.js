d9bd47468f3a21bad6f9892ead3151b8907b05ae
Eric Youngberg <eyoungberg@mapc.org>
2019-01-04 11:51:25 -0500

    Add additional layout to show stories page

--- app/javascript/packs/stories.js
+++ app/javascript/packs/stories.js
@@ -7,16 +7,18 @@ import HyperElement from '../classes/HyperElement';
 document.addEventListener('DOMContentLoaded', () => {
   const videoContainer = document.querySelector('.video-response');
 
-  const videoInfo = new HyperElement('p');
-  videoInfo.addClass('video-info');
+  if (videoContainer) {
+    const videoInfo = new HyperElement('p');
+    videoInfo.addClass('video-info');
 
-  const videoUploader = new HyperElement(videoContainer.querySelector('input[type="file"]'));
-  videoUploader.addEvent('change', () => {
-    const fileName = videoUploader.node.value.split('\\').pop();
+    const videoUploader = new HyperElement(videoContainer.querySelector('input[type="file"]'));
+    videoUploader.addEvent('change', () => {
+      const fileName = videoUploader.node.value.split('\\').pop();
 
-    videoInfo.setAttr('innerHTML', `Uploaded Video: ${fileName}`);
-    videoInfo.mount(videoContainer);
-  });
+      videoInfo.setAttr('innerHTML', `Uploaded Video: ${fileName}`);
+      videoInfo.mount(videoContainer);
+    });
+  }
 });
 
 
