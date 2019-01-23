d85dbefead7d52172b131059d6881ce395ab4172
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-01-23 11:24:49 -0500

    Resolve #154 Allow User to Select Question
    * Use styled buttons to allow user to pick one of three questions to respond to
    * Setup translation of questions using i18n pattern at https://stackoverflow.com/a/36335591/2149359
    * Add enum to Story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,6 +3,7 @@ class Story < ApplicationRecord
   has_one_attached :audio
   validate :has_attached_story
   after_save :create_transcript
+  enum question: [:question1, :question2, :question3]
 
   def has_attached_story
     if !audio.attached? && !video.attached? && response.blank?
