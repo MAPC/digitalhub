8b21e4bd9b61fe5a33efce2f7369342d0642cb90
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-02-07 10:49:13 -0500

    Update Weigh In Content

--- vendor/extensions/stories/app/helpers/refinery/stories/stories_helper.rb
+++ vendor/extensions/stories/app/helpers/refinery/stories/stories_helper.rb
@@ -5,7 +5,7 @@ module Refinery
         if enum === 'question1'
           'Congratulations! You get to ask someone in the year 2050 about what life is like! What do you want to know?'
         elsif enum === 'question2'
-          'In your dream future, what do you want life to be like? (Feel free to talk about specific groups like children or seniors, and specific issues like housing or transportation.)'
+          'In your ideal future, what do you want life to be like? (Feel free to talk about specific groups like children or seniors, and specific issues like housing or transportation.)'
         else
           'No question was selected.'
         end
