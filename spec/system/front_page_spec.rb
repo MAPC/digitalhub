29e4135d1724d1714dc70505d9365ea56f321e96
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-02-25 14:22:16 -0500

    Automatically Setup Chromedriver for macOS users
    This commit automatically sets up Chromedriver for macOS users.

--- spec/system/front_page_spec.rb
+++ spec/system/front_page_spec.rb
@@ -1,13 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Front Page", :type => :system do
-  context "changing languages" do
-
-    it "enables me to view the page in portugese", js: true do
-      create(:page)
-      visit "/"
-      expect(page).to have_text("MetroNext")
-    end
-
+  it "enables me to view the front page" do
+    create(:page)
+    visit "/"
+    expect(page).to have_text("Welcome to MetroCommon 2050!")
   end
 end
