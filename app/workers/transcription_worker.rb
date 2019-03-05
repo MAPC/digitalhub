1a2f0fac723dab2a9c80f8fa8ef09370f94c4e5a
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-03-05 13:48:22 -0500

    Add Transcription Worker Test
    * Add a working test for the Google transcription worker. This test mocks the Google API interaction using doubles in order to avoid hitting the actual Google API during the test suite execution.

--- app/workers/transcription_worker.rb
+++ app/workers/transcription_worker.rb
@@ -9,7 +9,7 @@ class TranscriptionWorker
              language_code:     "en-US"   }
     audio  = { uri: "gs://maps-digital-hub/#{story.audio.key}" }
 
-    operation = speech.long_running_recognize config, audio
+    operation = speech.long_running_recognize(config, audio)
     operation.wait_until_done!
     raise operation.results.message if operation.error?
 
--- spec/workers/transcription_worker_spec.rb
+++ spec/workers/transcription_worker_spec.rb
@@ -1,4 +1,38 @@
 require 'rails_helper'
+require "google/gax/operation"
 RSpec.describe TranscriptionWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "gets a transcription of the audio from Google" do
+    story = create(:story, :with_audio)
+    story.reload
+
+    operation = double('Google::Gax::Operation')
+    allow(operation).to receive(:wait_until_done!) { true }
+    allow(operation).to receive(:error?) { false }
+    allow(operation).to receive(:response) do
+      {"results" =>
+          [
+            {"alternatives" =>
+              [
+                {"transcript" => "the text in transcript"}
+              ]
+            }
+          ]
+        }
+    end
+
+    speech = double('Google::Cloud::Speech')
+    allow(speech).to receive(:long_running_recognize).with(hash_including(:encoding, :language_code), hash_including(:uri)) do
+      operation
+    end
+
+    allow(Google::Cloud::Speech).to receive(:new) { speech }
+    allow(Google::Gax::Operation).to receive(:new) { operation }
+
+    worker = TranscriptionWorker.new
+    worker.perform(story.id)
+
+    story.reload
+    expect(story.response.present?).to be_truthy
+    Sidekiq::Worker.clear_all
+  end
 end
