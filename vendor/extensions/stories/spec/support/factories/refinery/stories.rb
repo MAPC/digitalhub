4c8afc2c9d97cb64026faabd2a7da70f3810fc1e
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-03-05 13:48:22 -0500

    Add Transcription Worker Test
    * Add a working test for the Google transcription worker. This test mocks the Google API interaction using doubles in order to avoid hitting the actual Google API during the test suite execution.

--- vendor/extensions/stories/spec/support/factories/refinery/stories.rb
+++ vendor/extensions/stories/spec/support/factories/refinery/stories.rb
@@ -8,6 +8,9 @@ FactoryBot.define do
     trait :with_video do
       video { fixture_file_upload(Refinery::Stories.root.join('spec', 'support', 'assets', 'test-video.mp4'), 'video/mp4') }
     end
+
+    trait :with_audio do
+      audio { fixture_file_upload(Refinery::Stories.root.join('spec', 'support', 'assets', 'test-audio.wav'), 'audio/wav') }
+    end
   end
 end
-
