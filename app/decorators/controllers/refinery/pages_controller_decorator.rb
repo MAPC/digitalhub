ac24ee20433e07ac373e3fd2e32836be2e76cb2f
Eric Youngberg <eyoungberg@mapc.org>
2019-03-20 14:16:50 -0400

    Start formatting event-box content

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -7,13 +7,7 @@ Refinery::PagesController.class_eval do
     def find_next_events
       now = DateTime.now
       now_nozone = DateTime.new(now.year, now.month, now.day, now.hour, now.minute, now.second)
-      events = ::Refinery::Events::Event.where("start > ?", now_nozone).order('start ASC')
-
-      if events.size > 3
-          events = events[0...2]
-      end
-
-      @next_events = events
+      @next_events = ::Refinery::Events::Event.where("start > ?", now_nozone).order('start ASC')
     end
 
 end
