6a42f8242f6489f222b5b5278fd7124a9bb8fee8
Brad Smith <brad@smithwebtek.com>
2019-03-25 11:36:39 -0400

    Add image model relationship and image serializer

--- vendor/extensions/hero_images/app/controllers/refinery/hero_images/hero_images_controller.rb
+++ vendor/extensions/hero_images/app/controllers/refinery/hero_images/hero_images_controller.rb
@@ -11,9 +11,9 @@ module Refinery
         present(@page)
 
         hero_images = ::Refinery::HeroImages::HeroImage.all
-        # all_hero_images = hero_images.map { |hero_image| HeroImageSerializer.new(hero_image, { :include => [:image] }).serializable_hash }
-        # render json: all_hero_images
-        render json: hero_images
+        all_hero_images = hero_images.map { |hero_image| HeroImageSerializer.new(hero_image, { :include => [:image] }).serializable_hash }
+        render json: all_hero_images
+        # render json: hero_images
       end
 
       def show
--- vendor/extensions/hero_images/app/serializers/hero_image_serializer.rb
+++ vendor/extensions/hero_images/app/serializers/hero_image_serializer.rb
@@ -1,4 +1,5 @@
 class HeroImageSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :image, :url, :description, :format
+  has_one :image
 end
