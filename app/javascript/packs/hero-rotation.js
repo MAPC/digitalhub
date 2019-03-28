45e9210145cc96c638d70381df8246fa1e450422
Brad Smith <brad@smithwebtek.com>
2019-03-28 09:30:26 -0400

    Handle scenarios with baseUrl

--- app/javascript/packs/hero-rotation.js
+++ app/javascript/packs/hero-rotation.js
@@ -1,46 +1,75 @@
 $(() => {
-  getImages()
+  setTimeout(() => {
+    getImages()
+  }, 5000);
 });
 
 let counter = 0
 let imagesList = []
-let baseUrl = window.location.href.replace(/\/$/, "")
+let baseUrl = window.location.href
+
+if (baseUrl.match(/\/$/)) {
+  baseUrl = baseUrl.replace(/\/$/i, "")
+} if (baseUrl.match(/\/..?$/i)) {
+  baseUrl = baseUrl.replace(/\/..?$/i, "")
+  baseUrl = baseUrl.replace(/\/..?$/i, "")
+}
 
 const getImages = () => {
   $.get(`${baseUrl}/hero_images`)
     .done(response => {
-      counter = response.length - 1
+      counter = response.length
       imagesList = response
+      cacheImages(response)
       loadImages(response)
     })
 };
 
+const cacheImages = (images) => {
+  for (let i = 0; i < images.length; i++) {
+    $.get(images[i].included[0].attributes.hero_image_thumbnail_url)
+  }
+};
+
 const loadImages = (images) => {
   let index = 0
-  for (let i = 1; i < images.length; i++) {
+  for (let i = 0; i < images.length; i++) {
     setTimeout(() => {
     }, i * 1000);
 
     setTimeout(() => {
-      loadImage(images[i])
+      loadImage(images[i], images[i + 1])
 
-      if (index++ <= images.length) {
+      if (index < images.length) {
         loadImages(images[index]);
+        index++
       }
-    }, i * 5000);
+    }, i * 3000);
   }
 };
 
-const loadImage = (img) => {
-  let hero = $('.styled-box')[0];
+const loadImage = (imageCurrent, imageNext) => {
   counter -= 1
-  hero.style.backgroundImage = `url(${baseUrl}${img.included[0].attributes.hero_image_thumbnail_url})`
-  replay()
+  let hero = $('.styled-box')[0];
+
+  if (imageCurrent) {
+    let imgCurrentUrl = `${baseUrl}${imageCurrent.included[0].attributes.hero_image_thumbnail_url}`
+    hero.style.backgroundImage = `url(${imgCurrentUrl})`
+  }
+
+  if (imageNext) {
+    let imageNextUrl = `${baseUrl}${imageNext.included[0].attributes.hero_image_thumbnail_url}`
+    hero.style.backgroundImage = `url(${imageNextUrl})`
+  }
+
+  if (counter == 0) {
+    setTimeout(() => {
+      replay()
+    }, 5000);
+  }
 }
 
 const replay = () => {
-  if (counter == 0) {
-    counter = imagesList.length
-    loadImages(imagesList)
-  }
+  counter = imagesList.length
+  loadImages(imagesList)
 };
