5706c1260fe32311e50aacc047201ab99dd0a16a
smithwebtek <brad@smithwebtek.com>
2019-04-03 15:29:46 -0400

    Refactor tests

--- spec/system/hero_rotation_spec.rb
+++ spec/system/hero_rotation_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe "Hero image rotation", :type => :system do
   it "has a different hero section background image, 5 seconds after pageload, if 1 hero image is created", js: true do
     create(:page)
     hero_image = create(:hero_image)
-    window = Capybara.current_session.driver.browser.manage.window
-    window.resize_to(1440, 1000) # width, height    
+    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
     visit "/"
     sleep 3
     expect(page.find('div.rotation-test')[:style].split('/').last == ("beach.jpeg\");")).to be(true)
@@ -14,8 +13,7 @@ RSpec.describe "Hero image rotation", :type => :system do
   it "works with English language selected", js: true do
     create(:page)
     hero_image = create(:hero_image)
-    window = Capybara.current_session.driver.browser.manage.window
-    window.resize_to(1440, 1000) # width, height    
+    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
     visit "/en/"
     sleep 3
     expect(page.find('div.rotation-test')[:style].split('/').last == ("beach.jpeg\");")).to be(true)
@@ -24,8 +22,7 @@ RSpec.describe "Hero image rotation", :type => :system do
   it "works with Portugese language selected", js: true do
     create(:page)
     hero_image = create(:hero_image)
-    window = Capybara.current_session.driver.browser.manage.window
-    window.resize_to(1440, 1000) # width, height    
+    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
     visit "/pt/"
     sleep 3
     expect(page.find('div.rotation-test')[:style].split('/').last == ("beach.jpeg\");")).to be(true)
@@ -34,8 +31,7 @@ RSpec.describe "Hero image rotation", :type => :system do
   it "works with Spanish language selected", js: true do
     create(:page)
     hero_image = create(:hero_image)
-    window = Capybara.current_session.driver.browser.manage.window
-    window.resize_to(1440, 1000) # width, height    
+    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
     visit "/es/"
     sleep 3
     expect(page.find('div.rotation-test')[:style].split('/').last == ("beach.jpeg\");")).to be(true)
@@ -44,21 +40,17 @@ RSpec.describe "Hero image rotation", :type => :system do
   it "works with Chinese language selected", js: true do
     create(:page)
     hero_image = create(:hero_image)
-    window = Capybara.current_session.driver.browser.manage.window
-    window.resize_to(1440, 1000) # width, height    
+    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
     visit "/zh/"
     sleep 3
     expect(page.find('div.rotation-test')[:style].split('/').last == ("beach.jpeg\");")).to be(true)
   end
-
+  
   it "works with French language selected", js: true do
     create(:page)
-    hero_image1 = create(:hero_image)
-    hero_image2 = create(:hero_image)
-    hero_image3 = create(:hero_image)
-    window = Capybara.current_session.driver.browser.manage.window
-    window.resize_to(1440, 1000) # width, height    
+    hero_image = create(:hero_image)  
+    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
     visit "/fr/"
-    expect(page.has_selector?('.rotation-test')).to be(true)
+    expect(page.find('div.rotation-test')[:style].split('/').last == ("beach.jpeg\");")).to be(true)
   end
 end
