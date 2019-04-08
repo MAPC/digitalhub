09964b741350f0e3eaa2e0004f04c2f22f4a9d0e
smithwebtek <brad@smithwebtek.com>
2019-04-08 13:22:35 -0400

    Eric's refactor with 2 divs for image caching

--- app/javascript/packs/hero-rotation.js
+++ app/javascript/packs/hero-rotation.js
@@ -1,83 +1,56 @@
-$(() => {
-  if (checkWidth()) {
-    setTimeout(() => {
-      getImages()
-    }, 4000);
+window.addEventListener('load', () => {
+
+  const heroBackgroundSpeed = 5000;
+  const checkWidth = () => {
+    return document.documentElement.clientWidth > 965
   }
-});
 
-let counter = 0
-let imagesList = []
-let baseUrl = window.location.href
+  const getBaseURL = () => {
+    let baseUrl = window.location.href
 
-if (baseUrl.match(/\/$/)) {
-  baseUrl = baseUrl.replace(/\/$/i, "")
-} if (baseUrl.match(/\/..?$/i)) {
-  baseUrl = baseUrl.replace(/\/..?$/i, "")
-}
+    if (baseUrl.match(/\/$/)) {
+      baseUrl = baseUrl.replace(/\/$/i, "")
+    } if (baseUrl.match(/\/..?$/i)) {
+      baseUrl = baseUrl.replace(/\/..?$/i, "")
+    }
 
-const checkWidth = () => {
-  return document.documentElement.clientWidth > 965
-}
+    return baseUrl
+  };
 
-const getImages = () => {
-  $.get(`${baseUrl}/hero_images`)
-    .done(response => {
-      counter = response.length
-      imagesList = response
+  const getElements = (elements) => {
+    return [
+      elements.filter(el => el.classList.contains('active'))[0],
+      elements.filter(el => !el.classList.contains('active'))[0],
+    ];
+  };
+  const getImageElements = getElements.bind(null, Array.from(document.querySelectorAll('.hero-banner .styled-box__background-image')));
 
+  const getNextImage = (images, current) => {
+    const nextImageIndex = images.indexOf(current) + 1;
+    return images.length <= nextImageIndex ? 0 : nextImageIndex;
+  };
+
+  const renderNextImage = images => {
+    const [activeElem, inactiveElem] = getImageElements();
+    const nextImage = images[getNextImage(images, activeElem.style.backgroundImage)];
+    inactiveElem.style.backgroundImage = nextImage;
+
+    inactiveElem.classList.add('active');
+    activeElem.classList.remove('active');
+
+    if (checkWidth()) {
       setTimeout(() => {
-        loadImages(response)
-      }, 4000);
-    })
-};
+        renderNextImage(images)
+      }, heroBackgroundSpeed);
+    }
+  };
 
-const loadImages = (images) => {
-  let index = 0
-  for (let i = 0; i < images.length; i++) {
-    setTimeout(() => {
-      cacheImage(images[i])
-    }, i * 2000);
-
-    setTimeout(() => {
-      loadImage(images[i])
-
-      if (index < images.length) {
-        loadImages(images[index]);
-        index++
-      }
-    }, i * 6000);
-  }
-};
-
-const cacheImage = (img) => {
-  let imgUrl = `${baseUrl}${img.included[0].attributes.hero_image_thumbnail_url}`
-  let newImage = new Image()
-  newImage.src = imgUrl
-  $('div.image-cache')[0].style.backgroundImage = `url(${imgUrl})`
-}
-
-const loadImage = (img) => {
-  counter -= 1
-  let hero = $('.styled-box')[0];
-
-  if (img && checkWidth()) {
-    let imgUrl = `${baseUrl}${img.included[0].attributes.hero_image_thumbnail_url}`
-
-    hero.style.backgroundImage = `url(${imgUrl})`
-    hero.classList.add('rotation-test')
-  }
-
-  if (counter == 0) {
-    setTimeout(() => {
-      replay()
-    }, 5000);
-  }
-}
-
-const replay = () => {
-  counter = imagesList.length
-  if (checkWidth()) {
-    loadImages(imagesList)
-  }
-};
+  $.get(`${getBaseURL()}/hero_images`).done(response => {
+    const images = response.map(res => `url("${res.included[0].attributes.hero_image_thumbnail_url}")`);
+    if (checkWidth()) {
+      setTimeout(() => {
+        renderNextImage(images);
+      }, heroBackgroundSpeed)
+    }
+  });
+});
