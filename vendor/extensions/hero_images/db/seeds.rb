9cdc8b22204c662228fd98dbb50f9fa4cf3b5994
smithwebtek <brad@smithwebtek.com>
2019-04-09 12:48:33 -0400

    Refactor seeds.rb

--- vendor/extensions/hero_images/db/seeds.rb
+++ vendor/extensions/hero_images/db/seeds.rb
@@ -9,9 +9,9 @@ Refinery::I18n.frontend_locales.each do |lang|
 
   Dir[Rails.root.join('vendor', 'extensions', 'hero_images', 'lib', 'import', '*')].each do |file|
     if file
-      file_path = Rails.root.join(File.path(file).split('/').slice(6, 11).join('/'))
+      file_path = Rails.root.join(File.path(file))
       image = ::Refinery::Image.create(image: file_path)
-      hero_image = ::Refinery::HeroImages::HeroImage.create(title: image.image_name, description: image.image_name, image_id: image.id)
+      ::Refinery::HeroImages::HeroImage.create(title: image.image_name, description: image.image_name, image_id: image.id)
     else
       puts "ERROR: file error, not an image or not processable as an jpg image."
       puts "Please check app/vendor/extensions/hero_images/lib/import to confirm all files are of type: .jpg"
