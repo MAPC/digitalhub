2ec537802573bc7511ff5f68fa3fce5ff098b9cb
smithwebtek <brad@smithwebtek.com>
2019-04-11 14:12:27 -0400

    Change weigh in link to GOALS

--- app/javascript/packs/goals-survey.js
+++ app/javascript/packs/goals-survey.js
@@ -3,10 +3,12 @@ $(() => {
   oneBoxGoalsLink();
 })
 
+const goalsSurveyLink = "https://mapc.az1.qualtrics.com/jfe/form/SV_832YVvNk2Yabzox"
+
 const announcementGoalsLink = () => {
   $("body").on('click', 'a.button.announcements__button', function (event) {
     event.preventDefault();
-    if (document.documentElement.clientWidth > 770 && event.currentTarget.href == "https://mapc.az1.qualtrics.com/jfe/form/SV_0wiHeWbuN9GJ81f") {
+    if (document.documentElement.clientWidth > 770 && event.currentTarget.href == goalsSurveyLink) {
       openGoals()
     } else {
       openGoalsInNewTab(event.currentTarget.href);
@@ -17,7 +19,7 @@ const announcementGoalsLink = () => {
 const oneBoxGoalsLink = () => {
   $('a').on('click', (event) => {
     event.preventDefault();
-    if (document.documentElement.clientWidth > 770 && event.currentTarget.href == "https://mapc.az1.qualtrics.com/jfe/form/SV_0wiHeWbuN9GJ81f") {
+    if (document.documentElement.clientWidth > 770 && event.currentTarget.href == goalsSurveyLink) {
       openGoals()
     } else {
       openGoalsInNewTab(event.currentTarget.href);
@@ -30,7 +32,7 @@ const openGoals = () => {
   $('body').append(`
     <div class="overlay">
       <button id="close-survey" class="close-survey">&times;</button>
-      <iframe src="https://mapc.az1.qualtrics.com/jfe/form/SV_0wiHeWbuN9GJ81f" frameborder="0"; width=100%; height=auto; id="goals-survey";></iframe>
+      <iframe src="${goalsSurveyLink}" frameborder="0"; width=100%; height=auto; id="goals-survey";></iframe>
     </div>`)
   closeGoals();
 }
