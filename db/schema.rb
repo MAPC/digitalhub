db56304c0aa3f20fc4804da866bc50570ab37e5e
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-05-01 17:09:38 -0400

    Seed Weigh In Stories
    This commit seeds the weigh in stories into the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_185809) do
+ActiveRecord::Schema.define(version: 2019_05_01_202628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
