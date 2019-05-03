2024a7cb202255af181177a7368e57b56262f4b8
Brad Smith <brad@smithwebtek.com>
2019-05-03 15:01:28 -0400

    Response modal working
    * Refactor unique classes for response modal
    * Add 3 variables to response modal js
    * Get response modal working on non line-clamped responses
    * Update padding

--- spec/system/hero_rotation_spec.rb
+++ spec/system/hero_rotation_spec.rb
@@ -1,6 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "Hero image rotation", :type => :system do
+  # it "has a different hero section background image, 5 seconds after pageload, if 1 hero image is created", js: true do
+  #   create(:page)
+  #   create(:hero_image, image: build(:alternate_image))
+  #   Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
+  #   visit "/"
+  #   sleep 5
+  #   expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
+  # end
+
   it "has a different hero section background image, 5 seconds after pageload, if 1 hero image is created", js: true do
     create(:page)
     create(:hero_image, image: build(:alternate_image))
@@ -11,7 +20,7 @@ RSpec.describe "Hero image rotation", :type => :system do
     puts page.find(:xpath, '//*[@id="page"]/div/section[1]/div[1]/div[1]')['innerHTML']
     expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
   end
-
+  
   it "works with English language selected", js: true do
     create(:page)
     create(:hero_image, image: build(:alternate_image))
