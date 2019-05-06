58657643920b970f7857bc423adbd76b286ba99e
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-05-06 10:44:42 -0400

    Modify test to not test height
    Because we want to make this test less brittle and testing height is no longer relevant to the updated format of the weigh in page, we add this test to merely test for presenece of all the different kinds of stories on the weigh in page after they are created.

--- spec/system/weigh_in_stories_spec.rb
+++ spec/system/weigh_in_stories_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Weigh In Stories', :type => :system do
-  it 'sizes flexbox container correctly with 4 objects', js: :true do
+  it 'shows stories on the weigh in page', js: :true do
     create(:story, :with_video)
     create_list(:story, 2)
     create(:weigh_in_prompt, style: 'small')
     visit '/stories'
-    Capybara.current_session.driver.browser.manage.window.resize_to(1281, 1000)
-    expect(page).to have_xpath('//section[@id="stories"][@style="height: 784px;"]')
+    expect(page).to have_css('div .story', count: 4)
   end
 end
