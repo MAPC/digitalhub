d1715861ae1cbf43fc853f737ea649273b4a79d6
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-08-13 15:48:10 -0400

    Resolve #385 Implement External Translation Service
    * Update front-end to use Microsoft translation service.
    * Remove or revert AWS translation layer.
    * Remove language disclaimer and styles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def language(symbol)
+    languages = {
+      English: 'en',
+      Português: 'pt',
+      Español: 'es',
+      简体中文: 'zh-CHS',
+      Français: 'fr'
+    }
+    languages[symbol]
+  end
 end
--- vendor/extensions/events/app/models/refinery/events/event.rb
+++ vendor/extensions/events/app/models/refinery/events/event.rb
@@ -9,7 +9,6 @@ module Refinery
       validates :title, :presence => true, :uniqueness => true
       validates :start, :presence => true
       belongs_to :image, :class_name => '::Refinery::Image'
-      after_save :translate_content
 
       has_many :taggings, :class_name => '::Refinery::Taggings::Tagging', dependent: :destroy
       has_many :tags, :class_name => '::Refinery::Tags::Tag', through: :taggings
@@ -41,36 +40,6 @@ module Refinery
         next_three = ::Refinery::Events::Event.where('start > ?', DateTime.now).order(start: :asc).first(3)
         next_three_json = next_three.map {|event| EventSerializer.new(event, { :include => [:image] }).serializable_hash }
       end
-
-      protected
-
-        def translate_content
-          aws_translation_client = Aws::Translate::Client.new
-          auto_translate_mobility_obj(aws_translation_client, self)
-        end
-
-        def auto_translate_mobility_obj(client, obj)
-          translations_required = []
-          translatable_names = obj.translated_attribute_names.select { |name| !name.include?('slug') }
-          for attribute_name in translatable_names
-            for locale in Refinery::I18n.locales.keys
-              if obj.send("#{attribute_name}_en") && !obj.send("#{attribute_name}_#{locale}")
-                translations_required.push([attribute_name, locale])
-              end
-            end
-          end
-
-          for attribute_name, locale in translations_required
-            response = client.translate_text({
-              text: obj.send("#{attribute_name}_en"),
-              source_language_code: "en",
-              target_language_code: locale,
-            })
-            obj.translations.in_locale(locale).send("#{attribute_name}=", response.translated_text)
-            obj.translations.in_locale(locale).save!
-          end
-        end
-
     end
   end
 end
