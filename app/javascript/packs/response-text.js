85b56f263d6a703a88ac97be4bc779a07485a391
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-08-26 16:32:38 -0400

    Generalize truncation across all cards
    - Use localstorage to maintain and access original message
    - Remove references to Succinct

--- app/javascript/packs/response-text.js
+++ app/javascript/packs/response-text.js
@@ -1,30 +1,27 @@
 $(() => {
-  const origStoryText = $('div.story--response-text')[0].children[0].innerText
-  console.log(origStoryText)
   function reloadStoryTexts() {
     Array.from($('.story--response')).forEach((div) => {
       if (div.children[0].children[0]) {
         const modifiedDiv = div.children[0].children[0];
         modifiedDiv.innerText = window.localStorage.getItem(`${div.id}`);
       }
+      if (document.documentElement.clientWidth > 670) {
+        div.children[1].children[0].innerText = lineClampResponseTexts(div,350)
+      }
+      else if (document.documentElement.clientWidth < 670){
+        div.children[1].children[0].innerText = lineClampResponseTexts(div,55)
+      }
     });
   }
 
-  function lineClampResponseTexts() {
-      reloadStoryTexts();
-      if (document.documentElement.clientWidth > 670) {
-        let longStoryText = origStoryText.substring(0,350)
-        .split(' ')
-        .slice(0,-1)
-        .join(" ")
-        $('div.story--response-text')[0].children[0].innerText = longStoryText + "..."
-      } else if (document.documentElement.clientWidth < 670) {
-        let shortStoryText = origStoryText.substring(0,55)
-        .split(' ')
-        .slice(0,-1)
-        .join(" ")
-        $('div.story--response-text')[0].children[0].innerText = shortStoryText + "..."
-      }
+  function lineClampResponseTexts(div, length) {
+    const origText = localStorage.getItem(`${div.id}`)
+    let editedStoryText = origText.substring(0,length)
+      .split(' ')
+      .slice(0,-1)
+      .join(" ")
+      + "..."
+    return editedStoryText
     }
 
   function storeAllStoryTexts() {
@@ -37,9 +34,10 @@ $(() => {
           window.localStorage.setItem(`story${story.data.id}`, story.data.attributes.sanitized_response);
         }
       });
-      lineClampResponseTexts();
+      reloadStoryTexts()
     });
   }
+  
   storeAllStoryTexts();
-  window.addEventListener('resize', lineClampResponseTexts);
+  window.addEventListener('resize', reloadStoryTexts);
 });
