2ce0d4ac2936841fd75935d4ae44e22696f42ded
Brad Smith <brad@smithwebtek.com>
2019-08-26 14:40:39 -0400

    Style and populate menu
    * Add specific links to menu
    * Style for full column display
    * Style for main page scroll
    * Reduce transition time
    * Move toggleDrawer() from button to drawer itself
    * Refactor anonymous javascript arrow functions back to plain functions so IE 11 would work
    * Remove tinysort.js because it is not being used as originally intended

--- app/assets/javascripts/drawer.js
+++ app/assets/javascripts/drawer.js
@@ -9,7 +9,7 @@ function toggleDrawer() {
     // Drawer starts out aria-hidden
     // Hidden removes the menu from the tab order until the menu is opened
     // Timeout matches CSS transition
-    setTimeout(function() {
+    setTimeout(function () {
       if (!page.classList.contains("menu-open")) {
         drawer.hidden = true;
         drawer.setAttribute('aria-hidden', 'true');
@@ -19,7 +19,7 @@ function toggleDrawer() {
     drawer.hidden = false;
     drawer.setAttribute('aria-hidden', 'false');
     // Need time for hidden: false to take effect
-    setTimeout(function() {
+    setTimeout(function () {
       page.classList.add("menu-open");
       drawer.classList.add("menu-open");
       body.classList.add("menu-open");
