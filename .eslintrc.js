634f093c6f2b6dfea6be0f9ac4ef690c8e961c23
Matthew Zagaja <mzagaja@alum.wpi.edu>
2020-01-24 10:14:21 -0500

    Update max-len rule
    Update max-len rule to ignore Template Literals and Strings, and to respect the 2 character tab width we’ve been using (even though it doesn’t seem to yell about it, the default is 4).

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,11 @@ module.exports = {
   "rules": {
     "arrow-body-style": "off",
     "arrow-parens": "off",
+    "max-len": ['error', {
+      "ignoreStrings": true,
+      "tabWidth": 2,
+      "ignoreTemplateLiterals": true,
+      "ignoreUrls": true
+    }]
   }
 };
