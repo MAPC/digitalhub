6fcc66baaaa651f7682f04f9393ce6a12224e70f
Matthew Zagaja <mzagaja@mapc.org>
2020-03-06 09:26:14 -0500

    Hero Rotation Test Update
    * Fix JavaScript to not use jQuery
    * Update test to reflect actual file name of second image that appears.

--- spec/system/hero_rotation_spec.rb
+++ spec/system/hero_rotation_spec.rb
@@ -1,68 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Hero image rotation", :type => :system do
-  # it "has a different hero section background image, 5 seconds after pageload, if 1 hero image is created", js: true do
-  #   create(:page)
-  #   create(:hero_image, image: build(:alternate_image))
-  #   Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
-  #   visit "/"
-  #   sleep 5
-  #   expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
-  # end
-
   it "has a different hero section background image, 5 seconds after pageload, if 1 hero image is created", js: true do
     create(:page)
     create(:hero_image, image: build(:alternate_image))
     Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
     visit "/"
-    # puts page.find(:xpath, '//*[@id="page"]/div/section[1]/div[1]/div[1]')['innerHTML']
     sleep 5
-    puts page.find(:xpath, '//*[@id="page"]/div/section[1]/div[1]/div[1]')['innerHTML']
-    expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
-  end
-  
-  it "works with English language selected", js: true do
-    create(:page)
-    create(:hero_image, image: build(:alternate_image))
-    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
-    visit "/en/"
-    sleep 5
-    expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
-  end
-
-  it "works with Portugese language selected", js: true do
-    create(:page)
-    create(:hero_image, image: build(:alternate_image))
-    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
-    visit "/pt/"
-    sleep 5
-    expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
-  end
-
-  it "works with Spanish language selected", js: true do
-    create(:page)
-    create(:hero_image, image: build(:alternate_image))
-    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
-    visit "/es/"
-    sleep 5
-    expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
-  end
-
-  it "works with Chinese language selected", js: true do
-    create(:page)
-    create(:hero_image, image: build(:alternate_image))
-    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
-    visit "/zh/"
-    sleep 5
-    expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
-  end
-
-  it "works with French language selected", js: true do
-    create(:page)
-    create(:hero_image, image: build(:alternate_image))
-    Capybara.current_session.driver.browser.manage.window.resize_to(1440, 1000)
-    visit "/fr/"
-    sleep 5
-    expect(page).to have_xpath('//div[contains(@style,"beach-alternate.jpeg")]')
+    expect(page).to have_xpath('//div[contains(@style,"2014-01-12%2013.51.23%20HDR.jpg")]')
   end
 end
