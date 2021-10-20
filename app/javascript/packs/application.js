7c2edf39839322756fd6651477d3f94d99b6997e
TarinAComer <tcomer@mapc.org>
2021-10-20 10:21:09 -0400

    Trying to fix build error from regenerator-runtime

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,4 +6,5 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-import "babel-polyfill";
+import 'regenerator-runtime/runtime';
+import 'babel-polyfill';
